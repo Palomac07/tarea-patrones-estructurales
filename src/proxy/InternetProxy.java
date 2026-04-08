1d88894f1bc0484c720485796c5d8917db617f10
juligali <juligalianooo@gmail.com>
2026-04-08 10:35:00 -0300

    del proxy. main

--- src/proxy/InternetProxy.java
+++ src/proxy/InternetProxy.java
@@ -1,4 +1,29 @@
+
+
 import java.util.ArrayList;
 import java.util.List;
 
-Public 
+public class InternetProxy implements Internet {
+
+    private InternetReal internetReal; // tenes el objeto real
+    private List<String> sitiosProhibidos; // lista de paginas bloqueadas
+
+    public InternetProxy() {
+        internetReal = new InternetReal();
+
+        sitiosProhibidos = new ArrayList<>();
+        sitiosProhibidos.add("facebook.com");
+        sitiosProhibidos.add("instagram.com");
+        sitiosProhibidos.add("tiktok.com");
+    } // aca creo los sitios prohibidos
+
+    @Override
+    public void conectarA(String url) {
+
+        if (sitiosProhibidos.contains(url.toLowerCase())) { // aca se fija si la pagina esta en la lista de prohibido. toLowerCase convierte td en minuscula
+            System.out.println("Acceso Denegado a " + url);
+        } else {
+            internetReal.conectarA(url);
+        }
+    }
+}
